c194f6e66facb28e862e0250910347983631bf47
jh2416 <wogns4576@gmail.com>
2023-05-03 23:21:39 +0900

    #2 VARIABLES AND FUNCTIONS
    #2.2 Booleans and Strings

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-a = 2    #각각 메모리로 감 ex.0x0001
-b = 3
-c = a + b    #변수들로 새로운 변수를 만들 수 있음
-c = 1        #위에서 아래로 읽음 -> 1출력
-a = 1
-b = 10       #c는 안변함
-print(c)
+# a = 2    #각각 메모리로 감 ex.0x0001
+# b = 3
+# c = a + b    #변수들로 새로운 변수를 만들 수 있음
+# c = 1        #위에서 아래로 읽음 -> 1출력
+# a = 1
+# b = 10       #c는 안변함
+# print(c)
 
-my_age = 88
+# my_age = 88
+
+# Data types
+a = 654    #number
+print(a)
+my_name = "nico"    #text
+b = "1231231234"
+print(my_name)
+
+#boolean - True/False
+#대문자 필수
+c = True  #1, ON
+d = False #0, OFF
