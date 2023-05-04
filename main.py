3804f81d69e6d129121595e72675848385b3392d
jh2416 <wogns4576@gmail.com>
2023-05-04 16:52:18 +0900

    #2.5 Indentation
    #2 VARIABLES AND FUNCTIONS

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-# a = 2    #각각 메모리로 감 ex.0x0001
-# b = 3
-# c = a + b    #변수들로 새로운 변수를 만들 수 있음
-# c = 1        #위에서 아래로 읽음 -> 1출력
-# a = 1
-# b = 10       #c는 안변함
-# print(c)
+a = 2    #각각 메모리로 감 ex.0x0001
+b = 3
+c = a + b    #변수들로 새로운 변수를 만들 수 있음
+c = 1        #위에서 아래로 읽음 -> 1출력
+a = 1
+b = 10       #c는 안변함
+print(c)
 
-# my_age = 88
+my_age = 88
 
 #2.2
-# Data types
-# a = 654    #number
-# print(a)
-# my_name = "nico"    #text
-# b = "1231231234"
-# print(my_name)
+Data types
+a = 654    #number
+print(a)
+my_name = "nico"    #text
+b = "1231231234"
+print(my_name)
 
-# #boolean - True/False
-# #대문자 필수
-# c = True  #1, ON
-# d = False #0, OFF
+#boolean - True/False
+#대문자 필수
+c = True  #1, ON
+d = False #0, OFF
 
 #2.3
-# my_name = "hoon"
-# age = 27
-# dead = False
-# print("Hello my name is", my_name)
-# print("and I'm", age, "years old")
+my_name = "hoon"
+age = 27
+dead = False
+print("Hello my name is", my_name)
+print("and I'm", age, "years old")
 
 #2.4
 #function : 코드 조각 같은것, 반복해서 사용가능, print()도 function
@@ -43,4 +43,17 @@ def say_hello():
 
 say_hello()
 say_hello()
+say_hello()
+
+#2.5
+#indentation : 
+#코드가 어떤 것 안에 들어있는 것을 표시할때 2칸의 공백필요(자바 중괄호의 역할)
+
+def say_hello():
+  print("hello how r u?")
+
+def say_bye():
+  print("bye bye")
+  say_hello()
+
 say_hello()
