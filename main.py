a636d7dd5457f0f22ed5bb09f3fa48d1022529bb
jh2416 <wogns4576@gmail.com>
2023-05-11 17:19:19 +0900

    #3.6 Python Casino
    #3 CONTROL FLOW

--- main.py
+++ main.py
@@ -288,3 +288,22 @@ distance = 0
 while distance < 20:
   print("I'm running:", distance, "km")
   distance = distance + 1
+
+
+#3.6 Python Casino
+from random import randint  
+
+print("Welcome to Python Casino!")
+pc_choice = randint(1,50)
+
+playing = True
+
+while playing:
+  user_choice = int(input("Choose number: "))
+  if user_choice == pc_choice:
+    print("You won!")
+    playing = False
+  elif user_choice > pc_choice:
+    print("Lower!")
+  elif user_choice < pc_choice:
+    print("Higher!")
