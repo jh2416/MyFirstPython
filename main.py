7c2919f6f9035f1bf780576c29c5159ac760f81a
jh2416 <wogns4576@gmail.com>
2023-05-16 10:44:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -575,7 +575,7 @@ search_term = "python"
 #weworkremotely.com에서 python으로 검색한 페이지 접근
 response = get(f"{base_url}{search_term}")
 
-if response.status_code !== 200:
+if response.status_code != 200:
   print("Can't request website")
 #print(response) #확인
 else:
