756306c8a82c111ef6538e6e09bf05666e174991
jh2416 <wogns4576@gmail.com>
2023-05-23 23:55:00 +0900

    #5.15 Pages
    #5 JOB SCRAPPER

--- main.py
+++ main.py
@@ -955,3 +955,59 @@ for job in jobs:
     
 for result in results:
   print(results, "/////////\n//////////")
+
+
+#5.15 Pages(Pagination)
+# 다시한번 볼것
+from requests import get
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from extractor.wwr import extract_wwr_jobs
+from bs4 import BeautifulSoup
+
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+browser = webdriver.Chrome(options=options)
+
+def get_page_count(keyword):
+base_url = "https://kr.indeed.com/jobs?q="
+end_url = "&limit=50"
+browser.get(f"{base_url}{keyword}{end_url}")
+
+soup = BeautifulSoup(browser.page_source, "html.parser")
+pagination = soup.find("nav", class_="ecydgvn0")
+if pagination == None:
+return 1
+pages = pagination.find_all("div", recursive=False)
+print(len(pages))
+
+get_page_count("nest")
+
+
+def extract_indeed_jobs(keyword):
+base_url = "https://kr.indeed.com/jobs?q="
+end_url = "&limit=50"
+browser.get(f"{base_url}{keyword}{end_url}")
+
+results = []
+soup = BeautifulSoup(browser.page_source, "html.parser")
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+jobs = job_list.find_all('li', recursive=False)
+for job in jobs:
+zone = job.find("div", class_="mosaic-zone")
+if zone == None:
+anchor = job.select_one("h2 a")
+title = anchor['aria-label']
+link = anchor['href']
+company = job.find("span", class_="companyName")
+location = job.find("div", class_="companyLocation")
+job_data = {
+'link' : f"http://kr.indeed.com{link}",
+'company' : company.string,
+'location' : location.string,
+'position' : title
+}
+results.append(job_data)
+for result in results:
+print(result, "\n//////////\n")
